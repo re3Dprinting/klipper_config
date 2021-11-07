02afec426acf393bd2fcb491efc86fa61f0dc217
Noah Pan <noahpan323@gmail.com>
2021-11-07 14:42:47 -0600

    Add development pi upload option

--- upload_to_ip.sh
+++ upload_to_ip.sh
@@ -2,7 +2,7 @@
 
 usage() {
   cat << EOF # remove the space between << and EOF, this is due to web plugin issue
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [ip]
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [ip] [-d]
 
 Choose which Gigabot uploading config files to
 Upload klipper config files to RaspberryPi
@@ -10,25 +10,47 @@ Upload klipper config files to RaspberryPi
 Available options:
 
 -h, --help      Print this help and exit
-ip		Last octet of the machine to upload to. Assumes change FIRST_THREE_OCTETS to change ip octet.
+ip		          Last octet of the machine to upload to. Assumes change FIRST_THREE_OCTETS to change ip octet.
+-d              Upload development version of klipper_config
 EOF
   exit
 }
 
-if [ -z "$1" ]
+if [[ -z "$1" || $1 == "h" || $1 == "--help" ]]
 then
 	usage
 fi
 
+DEV=""
+if [[ -n "$2" && $2 == "-d" ]]
+then
+  echo Uploading to development pi
+  DEV=true
+  EXTRUDER_FILE="gigabot_extruders.cfg"
+  DEV_EXTRUDER_FILE=dev_"$EXTRUDER_FILE"
+  TMP="$EXTRUDER_FILE"_tmp
+fi
+
 FIRST_THREE_OCTETS="10.1.10."
 
-NEW_FILE="gigabot_steppers.cfg"
+STEPPER_FILE="gigabot_steppers.cfg"
 read -p "Enter Gigabot Size: (1)Regular (2)XLT (3)Terabot (4)Exabot: " model; echo
 ./get_bedsize.sh $model
 
+if [[ $DEV == "true" ]]
+then
+  mv $EXTRUDER_FILE $TMP
+  cp $DEV_EXTRUDER_FILE $EXTRUDER_FILE
+fi 
+
 echo Uploading to IP $FIRST_THREE_OCTETS$1
 scp -r {./*.cfg,./*.conf,./get_serial.sh} pi@$FIRST_THREE_OCTETS$1:~/klipper_config/
-rm $NEW_FILE
+rm $STEPPER_FILE
+
+if [[ $DEV == "true" ]]
+then
+  mv $TMP $EXTRUDER_FILE
+fi 
 
 ssh pi@$FIRST_THREE_OCTETS$1 "./klipper_config/get_serial.sh"
 
