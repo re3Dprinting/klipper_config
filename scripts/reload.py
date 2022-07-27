b7c9765072e02fe1eea678b2d4b9665fd417459a
Noah Pan <noahpan323@gmail.com>
2022-07-27 12:22:45 -0500

    WIP setup_scripts

--- scripts/reload.py
+++ scripts/reload.py
@@ -4,26 +4,29 @@ import requests
 import json
 import os
 import configparser
-from git import Repo
 from pathlib import Path
+
+import setup_printer
+from setup_printer import add_template_file, COMMON_PATH
+from branch_check import moonraker_klipper_branch_check
 
 url = "http://localhost"
 home_path = Path(__file__).parent.resolve().parent.parent
-klipper_path = home_path / "klipper"
-moonraker_path = home_path / "moonraker"
 
 klipper_config_path = home_path / "klipper_config"
-klipper_config_scripts = klipper_config_path / "scripts"
-master_config_path = klipper_config_path / ".master.cfg"
+fgf_config_path = klipper_config_path / "fgf"
+fff_config_path = klipper_config_path / "fff"
 
-master_config = configparser.ConfigParser(inline_comment_prefixes="#")
-master_config.read(str(master_config_path))
-master_config = master_config["re3D"]
+def read_master_config():
+    master_config_path = klipper_config_path / ".master.cfg"
+    if not master_config_path.exists():
+        print(".master.cfg is missing! autogenerating to default")
+        add_template_file(COMMON_PATH / "master.cfg", master_config_path)
 
-MASTER_BRANCH_VALID = {"stable", "develop"}
-KLIPPER_BRANCH_MAP = {"stable": "master", "develop": "develop"}
-MOONRAKER_BRANCH_MAP = {"stable": "master", "develop": "develop"}
-KLIPPER_CONFIG_BRANCH_MAP = {"stable": "", "develop": "-develop"}
+    master_config = configparser.ConfigParser(inline_comment_prefixes="#")
+    master_config.read(str(master_config_path))
+    return master_config["re3D"]
+    # for p in printer_config: print(p)
 
 def wait_on_moonraker():
     print("Waiting on moonraker...")
@@ -32,15 +35,6 @@ def wait_on_moonraker():
     retries = Retry(total=5, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
     s.mount('http://', HTTPAdapter(max_retries=retries))
     s.get(url + "/server/database/list")
-
-#Reloading serial script, Import script directly to run it.
-def trigger_setup_printer():
-    if (klipper_config_scripts / "setup_printer.py").is_file():
-        sys.path.append(str(klipper_config_scripts))
-        import setup_printer
-        setup_printer.main()
-    else:
-        print("Could not locate setup_printer.py in {}!".format(klipper_config_path))
 
 #Reloading UI configuration
 def reload_ui():
@@ -72,100 +66,19 @@ def check_network_availability():
         print("Network Unavailable, skipping dependency checking")
         return False
 
-def moonraker_klipper_branch_check():
-    branch = master_config.get("branch", fallback="stable")
-    print("Master Config Branch set to " + branch)
-    if branch not in MASTER_BRANCH_VALID: 
-        print("\t" +branch+ " is invalid, defaulting to stable")
-        branch = "stable"
-
-    target_klipper_branch = KLIPPER_BRANCH_MAP.get(branch)
-    target_moonraker_branch = MOONRAKER_BRANCH_MAP.get(branch)
-    print("Configuring klipper repo to " + target_klipper_branch)
-    print("Configuring moonraker repo to " + target_moonraker_branch)
-
-    print("Initializing repo objects...")
-    klipper_git_repo = Repo(klipper_path)
-    moonraker_git_repo = Repo(moonraker_path)
-
-    print("Fetching origin and pruning...")
-    klipper_git_repo.remotes.origin.fetch(prune=True)
-    moonraker_git_repo.remotes.origin.fetch(prune=True)
-
-    print("Checking if repos are dirty...")
-    klipper_repo_dirty = klipper_git_repo.is_dirty()
-    moonraker_repo_dirty = moonraker_git_repo.is_dirty()
-    print("\tKlipper -- {}".format(klipper_repo_dirty))
-    print("\tMoonraker -- {}".format(moonraker_repo_dirty))
-    if klipper_repo_dirty or moonraker_repo_dirty:
-        return False
-
-    # Check if branch exists
-    #TODO Actually check if the name matches by doing a .split("/")[-1]. 
-    for b in klipper_git_repo.remote().refs: 
-        if target_klipper_branch in b.name:
-            print("Klipper Target Branch exists...")
-    for b in moonraker_git_repo.remote().refs:
-        if target_moonraker_branch in b.name:
-            print("Moonraker Target Branch exists...")
-    
-    #Checkout Branch
-    klipper_git_repo.git.checkout(target_klipper_branch)
-    moonraker_git_repo.git.checkout(target_moonraker_branch)
-
-def klipper_config_branch_check():
-    branch = master_config.get("branch", fallback="stable")
-    print("Master Config Branch set to " + branch)
-    if branch not in MASTER_BRANCH_VALID: 
-        print("\t" +branch+ " is invalid, defaulting to stable")
-        branch = "stable"
-    extruder = master_config.get("extruder")
-    if extruder not in MASTER_EXTRUDER_VALID:
-        print("\t"+extruder+ " is not valid. Skipping klipper_config branch check")
-    
-    target_klipper_config_branch = "{}-{}".format(extruder, branch)
-    if branch is "stable": target_klipper_config_branch = extruder
-
-    print("Configuring klipper_config repo to " + target_klipper_config_branch)
-
-    print("Initializing repo objects...")
-    klipper_config_git_repo = Repo(klipper_config_path)
-
-    print("Fetching origin and pruning...")
-    klipper_config_git_repo.remotes.origin.fetch(prune=True)
-
-    print("Checking if repos are dirty...")
-    klipper_config_repo_dirty = klipper_config_git_repo.is_dirty()
-    print("\tKlipper_config -- {}".format(klipper_config_repo_dirty))
-    if klipper_config_repo_dirty:
-        return False
- 
-    for b in klipper_config_git_repo.remote().refs: 
-        if target_klipper_config_branch in b.name:
-            print("Klipper Target Branch exists...")
-    
-    #Checkout Branch
-    klipper_config_git_repo.git.checkout(target_klipper_config_branch)
-
-#Manage Moonraker, Klipper, virtual_keyboard version, based on klipper_config hashes
-#Fetch update manager status endpoint /machine/update/status?refresh=true
-
-#pull in klipper_config master.cfg, see if branch is on stable or develop, 
-# check out Moonraker, Klipper, klipper_config develop branches if set to develop
-
 def reboot_services():
     os.system("service moonraker restart")
     os.system("service klipper restart")
     os.system("service lightdm restart")
 
 def main():
+    master_config = read_master_config()
+    #Block until moonraker system service comes up. 
     wait_on_moonraker()
-
     if check_network_availability():
-        moonraker_klipper_branch_check()
-        # klipper_config_branch_check()
+        moonraker_klipper_branch_check(master_config=master_config)
 
-    trigger_setup_printer()
+    setup_printer.main()
     reload_ui()
     reboot_services()
 
--- scripts/setup_printer.py
+++ scripts/setup_printer.py
@@ -1,12 +1,19 @@
 #!/bin/python3
-import sys, shutil, configparser, subprocess
+import shutil
+import subprocess
 from pathlib import Path
 
 klipper_scripts = Path(__file__).parent.resolve()
-klipper_path = klipper_scripts.parent
-template_path = klipper_path / "templates"
-platform_path = klipper_path / "platform_type"
-board_path = klipper_path / "board_type"
+KLIPPER_PATH = klipper_scripts.parent
+COMMON_PATH = KLIPPER_PATH / "templates"
+PINMAP_PATH = KLIPPER_PATH / "board_pinmap"
+
+FGF_PATH = KLIPPER_PATH / "fgf"
+FFF_PATH = KLIPPER_PATH / "fff"
+platform_path = KLIPPER_PATH / "platform_specific"
+board_path = KLIPPER_PATH / "board_specific"
+
+OUTPUT_PATH = KLIPPER_PATH / "build"
 
 #attempt to generate master.cfg from master.cfg.tmpl
 #read in master.cfg
@@ -35,7 +42,7 @@ def add_platform_type(platform):
         print("Invalid platform type in master.cfg, defaulting to regular")
         platform = "regular"
 
-    generate_platform = klipper_path / "_platform_type.cfg"
+    generate_platform = KLIPPER_PATH / "_platform_type.cfg"
     template_platform = platform_path / (platform + ".cfg")
     add_template_file(template_platform, generate_platform, True) 
 
@@ -45,45 +52,27 @@ def add_board_type(board):
         print("Invalid board type in master.cfg, defaulting to archim")
         board = "archim"
 
-    generate_board_pinmap = klipper_path / "_board_pinmap.cfg"
-    generate_board_specific= klipper_path / "_board_specific.cfg"
+    board_pinmap_file = board + "_pinmap.cfg"
+    board_specific_file = board + "_specific.cfg"
+    generate_board_pinmap = OUTPUT_PATH / board_pinmap_file
+    generate_board_specific= OUTPUT_PATH / board_specific_file
     template_board_pinmap = board_path / (board + "_pinmap.cfg")
     template_board_specific = board_path / (board + "_specific.cfg")
     add_template_file(template_board_pinmap, generate_board_pinmap, True)
     add_template_file(template_board_specific, generate_board_specific, True)
 
-def main():
-    #Handle First Time .master.cfg Generation
-    master_config_path = klipper_path / ".master.cfg"
-    args = sys.argv
-    for arg in args:
-        if arg == "-g":
-            print("First time setup, generating .master.cfg")
-            add_template_file(template_path / "master.cfg", master_config_path)
-
-    if not master_config_path.exists():
-        print(".master.cfg is missing! autogenerating to default")
-        add_template_file(template_path / "master.cfg", master_config_path)
-
-    master_config = configparser.ConfigParser(inline_comment_prefixes="#")
-    master_config.read(str(master_config_path))
-    printer_config = master_config["re3D"]
-    # for p in printer_config: print(p)
-
+def main(printer_config):
     #Platform Setup
     add_platform_type(printer_config.get("platform_type", ""))
     #Board Setup
     add_board_type(printer_config.get("board_type", ""))
 
     #Template File Setup
-    add_template_file(template_path / "save_variables.cfg", klipper_path / "_save_variables.cfg")
-    add_template_file(template_path / "standalone.cfg", klipper_path / "_standalone.cfg")
-    add_template_file(template_path / "wifi_setup.conf.tmpl", klipper_path / "wifi_setup.conf")
-    add_template_file(template_path / "moonraker.conf.tmpl", klipper_path / "moonraker.conf", True)
+    add_template_file(COMMON_PATH / "save_variables.cfg", KLIPPER_PATH / "_save_variables.cfg")
+    add_template_file(COMMON_PATH / "standalone.cfg", KLIPPER_PATH / "_standalone.cfg")
+    add_template_file(COMMON_PATH / "wifi_setup.conf.tmpl", KLIPPER_PATH / "wifi_setup.conf")
+    add_template_file(COMMON_PATH / "moonraker.conf.tmpl", KLIPPER_PATH / "moonraker.conf", True)
 
     #Serial Setup
     serial_out = subprocess.run([str(klipper_scripts / "get_serial.sh")], capture_output=True)
     print(serial_out.stdout.decode("utf-8"))
-
-if __name__ == "__main__":
-    main()
