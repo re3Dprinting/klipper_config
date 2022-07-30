1ed1e98ba0affdd2b0d40e529452852e96c210e7
Noah Pan <noah.pan@classpass.com>
2022-07-30 13:54:02 -0400

    Fix serial script path

--- scripts/get_serial.sh
+++ scripts/get_serial.sh
@@ -2,7 +2,7 @@
 
 PWD="$(cd "$(dirname "$0")" && pwd)"
 KLIPPER_CONFIG_PWD=$PWD/..
-TMPL_PWD=$KLIPPER_CONFIG_PWD/templates
+TMPL_PWD=$KLIPPER_CONFIG_PWD/common
 
 AZTEEG_SERIAL="usb-FTDI_FT"
 ARCHIM_SERIAL="Klipper_sam3x8e"
@@ -30,4 +30,4 @@ fi
 echo "Detected serial: $serial"
 SERIAL_LINE='serial: .*'
 NEW_SERIAL_LINE="serial: $serial"
-sed --expression "s|$SERIAL_LINE|$NEW_SERIAL_LINE|g" $TMPL_PWD/serial.cfg > $KLIPPER_CONFIG_PWD/_serial.cfg
+sed --expression "s|$SERIAL_LINE|$NEW_SERIAL_LINE|g" $TMPL_PWD/serial.cfg > $KLIPPER_CONFIG_PWD/build/_serial.cfg
