28d80b1ba33dc80250576ea09f66e8df4f95e0ed
Noah Pan <noah.pan@classpass.com>
2022-09-18 15:08:08 -0500

    [system] Add upload firmware config file and update expect script

--- src/system_config/upload_firmware.sh
+++ src/system_config/upload_firmware.sh
@@ -2,5 +2,11 @@
 
 PWD="$(cd "$(dirname "$0")" && pwd)"
 
+# Check if a usb is plugged in
+# Check if the usb matches Klipper regex
+# flash and reboot if it doesnt match
+
+
 export LC_ALL=C
+cp $PWD/.config $HOME/klipper/.config
 $PWD/upload_firmware.exp
