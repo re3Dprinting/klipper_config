cc50fa6873c6e4231a90de0df48dc89b009bf756
Mitchell Mashburn <128167557+re3Dev@users.noreply.github.com>
2023-07-18 14:13:53 -0500

    Fix errors

--- src/system_config/integrate.sh
+++ src/system_config/integrate.sh
@@ -2,10 +2,10 @@
 
 cd ~
 mv ~/printer_data/config/standalone.cfg ~/standaloneTMP.cfg
-mv ~/printer_data/config/crowsnest.cfg ~/standaloneTMP.cfg
+mv ~/printer_data/config/crowsnest.conf ~/crowsnestTMP.conf
 mv ~/printer_data/config/timelapse.cfg ~/timelapseTMP.cfg
 rm -rf ~/printer_data/config
 cp -a ~/klipper_config/. ~/printer_data/config/
 mv ~/standaloneTMP.cfg ~/printer_data/config/standalone.cfg
-mv ~/crowsnestTMP.cfg ~/printer_data/config/crowsnest.cfg
+mv ~/crowsnestTMP.conf ~/printer_data/config/crowsnest.conf
 mv ~/timelapseTMP.cfg ~/printer_data/config/timelapse.cfg
