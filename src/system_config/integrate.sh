57f2003d005dc9b489d1ef0420304eefd08dc84a
Mitchell Mashburn <128167557+re3Dev@users.noreply.github.com>
2023-07-18 14:07:07 -0500

    Edit integrate script to preserve crowsnest and timelapse configs

--- src/system_config/integrate.sh
+++ src/system_config/integrate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd ~
-mv ~/printer_data/config/standalone.cfg ~/standaloneBKP.cfg
+mv ~/printer_data/config/standalone.cfg ~/standaloneTMP.cfg
+mv ~/printer_data/config/crowsnest.cfg ~/standaloneTMP.cfg
+mv ~/printer_data/config/timelapse.cfg ~/timelapseTMP.cfg
 rm -rf ~/printer_data/config
 cp -a ~/klipper_config/. ~/printer_data/config/
-mv ~/standaloneBKP.cfg ~/printer_data/config/standalone.cfg
+mv ~/standaloneTMP.cfg ~/printer_data/config/standalone.cfg
+mv ~/crowsnestTMP.cfg ~/printer_data/config/crowsnest.cfg
+mv ~/timelapseTMP.cfg ~/printer_data/config/timelapse.cfg
