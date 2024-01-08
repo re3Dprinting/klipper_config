fbd03e15f09efa539a295a75f028541d8a3df686
Mitchell-Mashburn <mitchell@re3d.org>
2024-01-08 13:07:56 -0600

    Changes for setup script

--- src/get_serial.sh
+++ src/get_serial.sh
@@ -6,27 +6,7 @@ TMPL_PWD=$PWD/common
 
 AZTEEG_SERIAL="usb-FTDI_FT"
 ARCHIM_SERIAL="Klipper_sam3x8e"
-
-if [[ -n "$1" && $1 == "-uart" ]]
-then
-    echo Using UART for com
-    serial="/dev/ttyAMA0"
-    # Need to append "restart_method: command" as well.
-else
-    serial=$(ls /dev/serial/by-id/* 2>/dev/null)
-    if [[ $? -ne 0 ]]
-    then
-        echo "No Gigabot Serial Detected, skipping..."
-        exit 0
-    fi
-
-    if [[ $serial != *"$AZTEEG_SERIAL"* ]] && [[ $serial != *"$ARCHIM_SERIAL"* ]]
-    then
-        echo "Serial found did not match serial regex match! serial: $serial"
-        exit 0
-    fi
-fi
-
+serial=$(ls /dev/serial/by-id/* 2>/dev/null)
 echo "Detected serial: $serial"
 SERIAL_LINE='serial: .*'
 NEW_SERIAL_LINE="serial: $serial"
