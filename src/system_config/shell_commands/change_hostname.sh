cb483046d7d8022775c4dfaae3d443944f6ed3b8
Mitchell-Mashburn <mitchell@re3d.org>
2024-03-21 11:00:02 -0500

    Fix typo

--- src/system_config/shell_commands/change_hostname.sh
+++ src/system_config/shell_commands/change_hostname.sh
@@ -3,7 +3,7 @@
 TIMESTAMP=$(date)
 file=/home/pi
 hostnamectl set-hostname "$1"
-echo "Hostname has been changed to: $1  - Interface will now connect with: $1.local - System needs to reboot for cha$
+echo "Hostname has been changed to: $1  - Interface will now connect with: $1.local - System needs to reboot for change to occur."
 echo "Timestamp: $TIMESTAMP" >> $file/shell_log.txt
 echo "Hostname has been changed to: $1" >> $file/shell_log.txt
 echo "" >> $file/shell_log.txt
