00380b8a5e72eff87ca8f306babe7b0512abaf59
Mitchell-Mashburn <mitchell@re3d.org>
2025-03-20 15:03:05 -0500

    Fixes for serial grab

--- src/get_serial.sh
+++ src/get_serial.sh
@@ -17,15 +17,11 @@ printer_serial=$(echo "$serial_devices" | grep -E "$AZTEEG_SERIAL|$ARCHIM_SERIAL
 # Find beacon serial
 beacon_serial=$(echo "$serial_devices" | grep "$BEACON_SERIAL")
 
-# Update printer serial in config
-SERIAL_LINE='serial: .*'
-NEW_SERIAL_LINE="serial: $printer_serial"
-sed --expression "s|$SERIAL_LINE|$NEW_SERIAL_LINE|g" $TMPL_PWD/serial.cfg > $KLIPPER_CONFIG_PWD/build/_serial.cfg
+# Update printer serial in config using the placeholder
+sed "s|{gigabot_serial}|$printer_serial|g" $TMPL_PWD/serial.cfg > $KLIPPER_CONFIG_PWD/build/_serial.cfg
 
 # If beacon is detected, create or update beacon config
 if [ ! -z "$beacon_serial" ]; then
     echo "Beacon detected: $beacon_serial"
-    BEACON_SERIAL_LINE='serial: .*'
-    NEW_BEACON_SERIAL_LINE="serial: $beacon_serial"
-    sed --expression "s|$BEACON_SERIAL_LINE|$NEW_BEACON_SERIAL_LINE|g" $TMPL_PWD/beacon.cfg > $KLIPPER_CONFIG_PWD/build/_beacon.cfg
+    sed --expression "s|serial:|serial: $beacon_serial|g" $TMPL_PWD/beacon.cfg > $KLIPPER_CONFIG_PWD/build/_beacon.cfg
 fi
