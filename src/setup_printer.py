070162fe90a9f518f92307d6c51b7a0d667fcffd
Mitchell-Mashburn <mitchell@re3d.org>
2025-03-20 15:27:30 -0500

    Make mesh compensation false by default, add to master.cfg

--- src/setup_printer.py
+++ src/setup_printer.py
@@ -92,7 +92,7 @@ def setup_fff_printer(printer_config, board, platform):
     custom_path = FFF_PATH / "custom"
     common_setup_printer(FFF_PATH, board, platform)
 
-    mesh_compensation_enabled = validate_and_return_config_param(field="mesh_compensation_enabled", config=printer_config, valid_selections=["true", "false"], default="true")
+    mesh_compensation_enabled = validate_and_return_config_param(field="mesh_compensation_enabled", config=printer_config, valid_selections=["true", "false"], default="false")
     if mesh_compensation_enabled == "true":
         add_template_file( custom_path / "fff_mesh.cfg", OUTPUT_PATH / "fff_mesh.cfg", False)
         add_template_file( custom_path / "fff_mesh_z.cfg", OUTPUT_PATH / "fff_mesh_z.cfg", False)
